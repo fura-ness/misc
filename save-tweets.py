ac6813caa244c2d12c8a799a2b5d6492fb6ba9bf
fura-ness <ness.fura@outlook.com>
2017-01-08 19:06:34 -0500

    Add files via upload

--- save-tweets.py
+++ save-tweets.py
@@ -1,38 +1,50 @@
 import time
 
+from bs4 import BeautifulSoup
 from splinter import Browser
 
-browser = Browser()
+OUTPUT_PATH = './tweet-images'
 
-#urls = """
-#https://twitter.com/Tommytoughstuff/status/517455614038257664
-#https://twitter.com/KattsDogma/status/724985571760824320
-#https://twitter.com/FrancyHam/status/621362196522467329
-#""".split()
+# people whose tweets i fave because they're friends
+USERS_FRIENDS = {'amgo','androidqueen','asoehnlen','chooch709','chrismkayser','CogoLabs','fishmcgill','FrappeTruckJim','JeffChausse','JessaBrez','jimmuhk','JoshArenstam','kfan','kman17','mossdogmusic','otterfamilias','rascalking','rubytoo','ScubaHey','TomAssBender'}
 
-tweets = [[f.strip() for f in line.split('\t')] for line in open('all-favorite-tweets.csv', 'r')]
-tweets = [('https://twitter.com%s' % tweet[0], tweet[1]) for tweet in tweets]
-
-friends = {'amgo','androidqueen','asoehnlen','chooch709','chrismkayser','CogoLabs','fishmcgill','FrappeTruckJim','JeffChausse','JessaBrez','jimmuhk','JoshArenstam','kfan','kman17','mossdogmusic','otterfamilias','rascalking','rubytoo','ScubaHey','tfederman','TomAssBender'}
-misc = {'StellaMadeline1','seldo','fuzzybinary'}  # seldo gone?
-
-for n, (url, timestamp) in enumerate(tweets):
-
-    user, _, tweet_id = url.split('/')[3:6]
-    print n, user, tweet_id
-
-    browser.visit(url)
-
-    dir = 'tweets-public'
-    if user in friends:
-        dir = 'tweets-friends'
-    elif user in misc:
-        dir = 'tweets-misc'
-
-    browser.driver.save_screenshot('%s/tweet-%s-%s-%s.png' % (dir, user, timestamp, tweet_id))
-
-    time.sleep(2)
-
-browser.quit()
+# non-friend, non-comedy accounts
+USERS_MISC    = {'StellaMadeline1','seldo','fuzzybinary','tfederman'}
 
 
+if __name__=='__main__':
+
+    browser = Browser()
+    soup = BeautifulSoup(open('faves.html').read(), 'html.parser')
+
+    faves = []
+    for a in soup.find_all("a"):
+        href = a.get('href')
+        data_time = None
+        for s in a.find_all('span'):
+            data_time = s.get('data-time', data_time)
+
+        if href and data_time and '/status' in href:
+            faves.append((href, data_time))
+
+
+    for n, (url, timestamp) in enumerate(faves):
+
+        url = 'https://twitter.com%s' % url
+        user, _, tweet_id = url.split('/')[3:6]
+
+        print '%03d/%03d' % (n+1, len(faves))
+
+        if user in USERS_FRIENDS:
+            dir = 'tweets-friends'
+        elif user in USERS_MISC:
+            dir = 'tweets-misc'
+        else:
+            dir = 'tweets-public'
+
+        output_path = '%s/%s' % (OUTPUT_PATH, dir)    
+
+        browser.visit(url)
+        browser.driver.save_screenshot('%s/tweet-%s-%s-%s.png' % (output_path, user, timestamp, tweet_id))
+
+        time.sleep(2)
